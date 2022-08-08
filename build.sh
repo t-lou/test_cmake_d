debe34ce19417b91d2878d94e125ff0435c6cd48
Lou <tongxi.lou@gmail.com>
2022-08-09 00:08:12 +0200

    move prep to build

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 BASE=$(dirname $(readlink -f "$0"))
 PROJ=${BASE}/Project
 
+[ ! -d "${BASE}/cmake-d" ] && git clone https://github.com/dcarp/cmake-d.git ${BASE}/cmake-d
+
 rm -rf ${PROJ}/build
 mkdir ${PROJ}/build
 cd ${PROJ}/build
